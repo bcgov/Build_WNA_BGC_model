5fa3381f03f70517e40172ad28c48c3fc354c626
CeresBarros <ceres.barros@gov.bc.ca>
2024-01-11 14:41:25 -0800

    minor

--- R/utils.R
+++ R/utils.R
@@ -37,7 +37,7 @@ makePointCoords <- function(bgc_poly, elev, gridSize = 2000, crs = "EPSG:4326") 
   bgc_grid2 <- vect(bgc_grid)
   tmp_elev <- extract(elev, bgc_grid2)
   
-  coords <- geom(bgc_grid2, df = T)
+  coords <- geom(bgc_grid2, df = TRUE)
   
   setDT(coords)
   coords[, c("part","hole","geom") := NULL]
