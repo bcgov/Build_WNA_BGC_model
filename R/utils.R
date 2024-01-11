94e68f0d3f0516f4f8cc842b97fffdaa57b7b42f
CeresBarros <ceres.barros@gov.bc.ca>
2024-01-11 11:21:46 -0800

    document functions

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,18 @@
+#' Prepares coordinates and obtains climate normals
+#'  using `climr_downscale`
+#'
+#' @param coords a `data.table` with point coordinates ("x" = longitude,
+#'   "y" = latitude), elevation ("elev") and point IDs ("id").
+#' @param bgcs a polygon `sf` object of biogeoclimatic zones to train the model. 
+#' @param ... further arguments passed to [climr_downscale()].
+#' 
+#' @seealso [climr_downscale()]
+#'
+#' @return climate normals as a `data.table`
+#' @export
+#'  
+#' @importFrom sf st_as_sf  st_join
+#' @importFrom data.table data.table
 getClimate <- function(coords, bgcs, ...) {
   dots <- list(...)
   
@@ -15,6 +30,15 @@ getClimate <- function(coords, bgcs, ...) {
   return(out)
 }
 
+
+#' Wrapper for `climr_downscale`
+#'
+#' @inheritParams getClimate 
+#'
+#' @return climate normals as a `data.table`.
+#' 
+#' @importFrom climr climr_downscale
+#' @importFrom data.table setDT
 .getClimVars <- function(coords, coords_bgc, ...) {
   clim_vars <- climr_downscale(coords, ...)
   setDT(clim_vars)
@@ -27,6 +51,28 @@ getClimate <- function(coords, bgcs, ...) {
   return(clim_vars)
 }
 
+#' Add extra climate variables
+#'
+#' @param dat a `data.table` with columns:
+#'    * PPT05, PPT06, PPT07, PPT08, PPT09 (May, ..., September precip), 
+#'    PPT_at (autumn precip), PPT_wt (winter precip)
+#'    * CMD07 (July climate moisture deficit), CMD (annual CMD)
+#'    * DD_0_at (autumn degree-days below 0 deg), DD_0_wt (winter degree-days below 0 deg)
+#'    
+#' @details This function calculates more climate variables derived from those
+#'   output by `climr_downscale`. Presently it adds the following:
+#'   *   May to June precip.: \eqn{PPT_MJ = PPT05 + PPT06}
+#'   *   July to September precip \eqn{PPT_JAS = PPT07 + PPT08 + PPT09}
+#'   *   Precipitation during vegetation dormant period: \eqn{PPT.dormant = PPT_at + PPT_wt}
+#'   *   CMD deficit \eqn{CMD.def = 500 - PPT.dormant} (bounded to 0)
+#'   *   \eqn{CMDMax = CMD07}
+#'   *   \eqn{CMD.total = CMD.def + CMD}
+#'   *   \eqn{DD_delayed = ((DD_0_at + DD_0_wt)*0.0238) - 1.8386} bounded to 0)
+#'  
+#' @return `dat` with all of the above added climate variables.
+#' @export
+#'
+#' @examples
 addVars <- function(dat) {
   dat[, PPT_MJ := PPT05 + PPT06]
   dat[, PPT_JAS := PPT07 + PPT08 + PPT09]
@@ -39,7 +85,26 @@ addVars <- function(dat) {
   dat[DD_delayed < 0, DD_delayed := 0]
 }
 
-# Function to log-transform fields in a data.table (e.g., climr output)
+#' Log-transform climate variables
+#'
+#' @param dat a `data.table` with columns of climate variables corresponding to 
+#'   the selected climate `elements`.
+#' @param elements character. Climate elements to search for in `dat`.
+#' @param base numeric. Log base.
+#' @param add.vars logical. If `TRUE`, the new logged variables are added to `dat` 
+#'   (TRUE). Otherwise, original column values are replaced with the logs (FALSE).
+#' @param zero_adjust logical. If `TRUE` adjusts zeroes in raw data as:
+#'   \eqn{base^{\log_base{x_min} - 1}.
+#'   where \eqn{x_min} is the minimum non-zero, non-NA value.
+#'
+#' @details
+#'   All column names that partially match strings in `elements` will be 
+#'   log-transformed.
+#' 
+#' @return
+#' @export
+#'
+#' @examples
 logVars <- function(dat,
                     elements = c("AHM", "DD", "Eref", "FFP", "NFFD", "PAS", "PPT", "SHM", "CMI"),
                     base = exp(1),
@@ -78,6 +143,20 @@ logVars <- function(dat,
 
 
 removeOutlier <- function(dat, alpha,numIDvars){
+#'
+#' @param dat a `data.table` target data to "clean"
+#' @param alpha numeric. The alpha value used to determine the cutoff for outliers.
+#' @param IDvars character. Names of columns from which outliers should be excluded.
+#' 
+#' @details TODO. Parallelizes computations internally with `foreach`.
+#'
+#' @return
+#' @seealso [foreach::foreach()]
+#' 
+#' @importFrom foreach foreach %do%
+#' @importFrom stats mahalanobis qchisq cov
+#' 
+#' @export
   out <- foreach(curr = unique(as.character(dat$BGC)), .combine = rbind) %do% {
     temp <- dat[dat$BGC == curr,]
     md <- tryCatch(mahalanobis(temp[,-c(1:numIDvars)],
