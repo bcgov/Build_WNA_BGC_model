6915015e7b4817000ab0dc5be2bb0fb5e86e487c
MacKenzie <Will.MacKenzie@gov.bc.ca>
2024-02-24 14:48:07 -0800

    changes to run on will's laptop

--- R/LocalFeatureSelection.R
+++ R/LocalFeatureSelection.R
@@ -45,12 +45,14 @@ BC_BGCs <- bgc_info[grep("BGC.*",Source),BGC]
 # saveRDS(neighbours_ls,"BC_BGC_NeighbourList.rds",)
 # st_write(bgc_map, "BC_BGCs_with_ID.gpkg")
 bgc_map <- st_read("BC_BGCs_with_ID.gpkg")
-dem <- terra::rast("../Common_Files/WNA_DEM_SRT_30m_cropped.tif")
-#dem.noram <- rast("//objectstore2.nrs.bcgov/ffec/DEM/DEM_NorAm/NA_Elevation/data/northamerica/northamerica_elevation_cec_2023.tif")
+dem <- terra::rast("D:/DEMs/WNA_DEM_4326_clipped.tif")# %>% terra::project("epsg:4326")
+#writeRaster(dem, "D:/DEMs/WNA_DEM_4326_clipped.tif")
+#dem <- rast("//objectstore2.nrs.bcgov/ffec/DEM/DEM_NorAm/NA_Elevation/data/northamerica/northamerica_elevation_cec_2023.tif")
 #dem <- dem.noram
 
 neighbours_ls <- readRDS("BC_BGC_NeighbourList.rds")
-vars.selected <- fread("no_month_vars.csv")
+remove = c("PPT_at", "PPT_wt", "CMD.def")
+vars.selected <- fread("no_month_vars.csv") %>% dplyr::filter(!vars %in% remove)
 
 bgc_list <- names(neighbours_ls)
 bgc_list <- bgc_list[!grepl("BAFA.* | *.un.", bgc_list)]
@@ -59,10 +61,10 @@ bgc_list <- bgc_list[-1]
 tic()
 
 vars <- c(vars.selected$vars,"BGC")
-bgc_list_short <- c("BGxh3", "BWBSdk", "CDFmm", "CWHvm2", "ESSFdk1","ICHmw1","IDFdk3",
-                    "IDFxx2","MHmm2","MSdw","SBSdk","SWBmk")
-#bgc_list <- bgc_list[-"CWHvm2"]
-
+bgc_list_short <- c("BGxh3", "BWBSdk", "CDFmm", "ICHmc1", "CWHmm1", "ESSFwk1","ICHmw1","IDFdk3",
+                    "IDFxx2","MSdw","SBSdk","MSxv")
+bgc_list <- bgc_list[-"CWHvm2"]
+tic()
 res_list <- list()
 for(bgc in bgc_list_short){
   cat("Processing",bgc,"\n")
@@ -75,7 +77,7 @@ for(bgc in bgc_list_short){
   pnts_all <- st_transform(pnts_all, 4326)
   coords <- st_coordinates(pnts_all)
   temp_elev <- terra::extract(dem, coords)
-  coords <- data.frame(coords, elev = temp_elev$WNA_DEM_SRT_30m,
+  coords <- data.frame(coords, elev = temp_elev$WNA_DEM_3005_clipped,
                        ID = 1:nrow(pnts_all), BGC = pnts_all$BGC)
   
   # coords <- data.frame(coords, elev = temp_elev$WNA_DEM_SRT_30m, 
@@ -87,7 +89,7 @@ for(bgc in bgc_list_short){
   addVars(clim_vars)
   clim_vars <- clim_vars[,..vars]
   clim_vars[,BGC := as.factor(BGC)]
-  rf_mod <- ranger(BGC ~ ., data = clim_vars, num.trees = 201, importance = "impurity", splitrule = "gini")
+  rf_mod <- ranger(BGC ~ ., data = clim_vars, num.trees = 101, importance = "impurity", splitrule = "gini")
   varimp <- sort(importance(rf_mod),decreasing = T)[1:10]
   res_list[[bgc]] <- data.table(Focal = bgc, Var = names(varimp), 
                                 Importance = unname(varimp), 
@@ -96,11 +98,11 @@ for(bgc in bgc_list_short){
 }
 
 
-dat_all <- rbindlist(res_list)
+dat_all2 <- rbindlist(res_list)
 fwrite(dat_all, "Focal_Variable_Importance_v2.csv")
 test <- dat_all[,.(Num = .N), by = .(Var)]
 setorder(test, -Num)
-# toc()
+toc()
 # 
 # 
 # st_write(out, "Test_IDFdk3_Neighbours.gpkg")
